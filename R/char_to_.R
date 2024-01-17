545640c295420407f71a58e34df0af56283d181d
CJ Yetman <cyetman@rmi.org>
2024-01-17 12:33:35 +0100

    Update char_to_.R

--- R/char_to_.R
+++ R/char_to_.R
@@ -2,9 +2,9 @@
 # characters using hex or unicode R escape strings
 
 char_to_hex <- function(x) {
-  cat(paste(paste0("\\x", charToRaw(x)), collapse = ""))
+  cat(paste0("\"", paste0("\\x", charToRaw(x), collapse = ""), "\""))
 }
 
 char_to_unicode <- function(x) {
-  cat(paste0("\\U", format(as.hexmode(utf8ToInt(x)), width = 2 * length(charToRaw(x)), upper = TRUE)))
+  cat(paste0("\"\\U", format(as.hexmode(utf8ToInt(x)), width = 8, upper = TRUE), "\""))
 }
